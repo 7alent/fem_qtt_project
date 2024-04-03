09e11b7dd5ff287b5175bf96ae742639f9270e8b
7alent <64833766+7alent@users.noreply.github.com>
2024-04-03 23:07:08 +0800

    Update fem_norm.m

--- fem_norm.m
+++ fem_norm.m
@@ -1,7 +1,7 @@
 % 计算FEM求得的两个解向量对应解函数的误差范数
 % V, W 为解向量(二维数组)
 % P 为节点坐标信息矩阵, 第n列为第n个节点的坐标
-% norm_type 为范数类型: "L2" 或 "H1"
+% norm_type 为范数类型: 'L2' 或 'H1'
 % 若要求V(W)的范数, 设定W(V)为0向量即可
 % 暂时只支持[-1, 1]上的函数
 
@@ -18,10 +18,10 @@ function output = fem_norm(W, V, norm_type)
     W_sz = size(W, 1);
     V_sz = size(V, 1);
     if W_sz < V_sz
-        U = interp1(linspace(-1, 1, W_sz), W, linspace(-1, 1, V_sz), "linear");
+        U = interp1(linspace(-1, 1, W_sz), W, linspace(-1, 1, V_sz), 'linear');
         U = U'-V;
     elseif W_sz > V_sz
-        U = interp1(linspace(-1, 1, V_sz), V, linspace(-1, 1, W_sz), "linear");
+        U = interp1(linspace(-1, 1, V_sz), V, linspace(-1, 1, W_sz), 'linear');
         U = U'-W;
     else
         U = W-V;
@@ -41,12 +41,12 @@ function output = fem_norm(W, V, norm_type)
     slope = (U(index+1)-U(index))/h; % 高斯点所在单元的解函数斜率
     U_gauss = (gauss_point-left_node).*slope+U(index); % 解函数在高斯点处的值
     output = dot(weight, U_gauss.^2);
-    if norm_type == "H1"
+    if norm_type == 'H1'
         U_d = U; % (近似)一阶导数
         U_d(2:N_m-1) = (U_d(3:N_m)-U_d(1:N_m-2))/(2*h);
         U_d(1) = U_d(2);
         U_d(N_m) = U_d(N_m-1);
-        output = sqrt(output+fem_norm(U_d, 0.*U_d, "L2")^2);
+        output = sqrt(output+fem_norm(U_d, 0.*U_d, 'L2')^2);
     else
         output = sqrt(output);
     end
